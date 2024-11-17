04f6e4f3bddee0851f948181f70ce1a0c67f604a
anotherhollow1125 <anotherhollow1125@gmail.com>
2024-11-17 12:09:17 +0000

    feat: builder/tests/02-create-builder.rs その1

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,9 +1,15 @@
 use proc_macro::TokenStream;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _input = parse_macro_input!(input as DeriveInput);
+    let DeriveInput { ident, .. } = parse_macro_input!(input as DeriveInput);
 
-    TokenStream::new()
+    quote! {
+        impl #ident {
+            pub fn builder() {}
+        }
+    }
+    .into()
 }
