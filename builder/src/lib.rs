2352fbb28fbe253d0ba590b211b77ad397a51f93
anotherhollow1125 <anotherhollow1125@gmail.com>
2024-11-19 02:32:24 +0000

    feat: builder/tests/06-optional-field.rs

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
-use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput};
+use quote::{format_ident, quote, ToTokens};
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Field, PathArguments, Type, TypePath};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -21,12 +21,33 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let mut builder_to_struct = vec![];
 
     for field in fields {
-        let vis = field.vis;
-        let ident = field.ident.unwrap();
-        let ty = field.ty;
+        let Field {
+            vis,
+            ident: Some(ident),
+            ty,
+            ..
+        } = field.clone()
+        else {
+            panic!("Invalid Structure.");
+        };
+
+        let (typ, is_option) = match ty {
+            Type::Path(TypePath { qself: None, path })
+                if path.segments.first().map(|s| &s.ident) == Some(&format_ident!("Option")) =>
+            {
+                let PathArguments::AngleBracketed(aga) =
+                    path.segments.into_iter().next().unwrap().arguments
+                else {
+                    panic!("Invalid Option Structure.");
+                };
+
+                (aga.args.to_token_stream(), true)
+            }
+            ty => (ty.to_token_stream(), false),
+        };
 
         builder_fields.push(quote! {
-            #vis #ident: ::std::option::Option<#ty>,
+            #vis #ident: ::std::option::Option<#typ>,
         });
 
         builder_fields_for_new.push(quote! {
@@ -34,19 +55,28 @@ pub fn derive(input: TokenStream) -> TokenStream {
         });
 
         methods.push(quote! {
-            fn #ident(&mut self, #ident: #ty) -> &mut Self {
+            fn #ident(&mut self, #ident: #typ) -> &mut Self {
                 self.#ident = ::std::option::Option::Some(#ident);
                 self
             }
         });
 
-        builder_to_struct.push(quote! {
-            #ident: self.#ident
-                .take()
-                .ok_or_else(|| ::std::boxed::Box::<dyn ::std::error::Error>::from(
-                    ::std::format!("'{}' is not set.", ::std::stringify!(#ident))
-                ))?,
-        });
+        let ts = if is_option {
+            quote! {
+                #ident: self.#ident
+                    .take(),
+            }
+        } else {
+            quote! {
+                #ident: self.#ident
+                    .take()
+                    .ok_or_else(|| ::std::boxed::Box::<dyn ::std::error::Error>::from(
+                        ::std::format!("'{}' is not set.", ::std::stringify!(#ident))
+                    ))?,
+            }
+        };
+
+        builder_to_struct.push(ts);
     }
 
     quote! {
