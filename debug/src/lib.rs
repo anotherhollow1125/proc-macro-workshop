e35c39078b6695be71b36e2166e47a5c28e685d1
anotherhollow1125 <anotherhollow1125@gmail.com>
2024-11-23 17:52:38 +0000

    feat: debug/tests/02-impl-debug.rs

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,55 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Error, Field, Fields, Ident, Result};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let derive_input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let DeriveInput {
+        ident,
+        data: Data::Struct(DataStruct { fields, .. }),
+        ..
+    } = derive_input
+    else {
+        return Error::new(
+            derive_input.ident.span(),
+            "Invalid Target: this macro can be used for only structs.",
+        )
+        .into_compile_error()
+        .into();
+    };
+
+    derive_inner(ident, fields)
+        .unwrap_or_else(Error::into_compile_error)
+        .into()
+}
+
+fn derive_inner(ident: Ident, fields: Fields) -> Result<TokenStream2> {
+    let fields = fields
+        .into_iter()
+        .map(|field| {
+            let Field {
+                ident: Some(ident), ..
+            } = field
+            else {
+                return Err(Error::new(ident.span(), "Expected named fields."));
+            };
+
+            Ok(quote! { .field(::std::stringify!(#ident), &self.#ident) })
+        })
+        .collect::<Result<Vec<_>>>()?;
+
+    Ok(quote! {
+        impl ::std::fmt::Debug for #ident {
+            fn fmt(&self, fmt: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+                fmt.debug_struct(::std::stringify!(#ident))
+                    #(
+                        #fields
+                    )*
+                    .finish()
+            }
+        }
+    })
 }
---
+++ debug/src/main.rs
@@ -0,0 +1,20 @@
+// for play ground.
+
+use derive_debug::CustomDebug;
+
+#[derive(CustomDebug)]
+pub struct Field {
+    name: &'static str,
+    bitmask: u8,
+}
+
+fn main() {
+    let f = Field {
+        name: "F",
+        bitmask: 0b00011100,
+    };
+
+    let debug = format!("{:?}", f);
+
+    assert!(debug.starts_with(r#"Field { name: "F","#));
+}
