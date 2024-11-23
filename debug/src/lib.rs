488a69fddae0dd509d07d9117a86d9ede87453e8
anotherhollow1125 <anotherhollow1125@gmail.com>
2024-11-23 18:51:22 +0000

    feat: debug/tests/03-custom-format.rs

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,9 +1,13 @@
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Error, Field, Fields, Ident, Result};
+use syn::spanned::Spanned;
+use syn::{
+    parse_macro_input, Data, DataStruct, DeriveInput, Error, Expr, ExprLit, Field, Fields, Ident,
+    Lit, Meta, MetaNameValue, Result,
+};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let derive_input = parse_macro_input!(input as DeriveInput);
 
@@ -29,16 +33,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
 fn derive_inner(ident: Ident, fields: Fields) -> Result<TokenStream2> {
     let fields = fields
         .into_iter()
-        .map(|field| {
-            let Field {
-                ident: Some(ident), ..
-            } = field
-            else {
-                return Err(Error::new(ident.span(), "Expected named fields."));
-            };
-
-            Ok(quote! { .field(::std::stringify!(#ident), &self.#ident) })
-        })
+        .map(field_method)
         .collect::<Result<Vec<_>>>()?;
 
     Ok(quote! {
@@ -52,4 +47,50 @@ fn derive_inner(ident: Ident, fields: Fields) -> Result<TokenStream2> {
             }
         }
     })
+}
+
+fn field_method(field: Field) -> Result<TokenStream2> {
+    let Field {
+        ident: Some(ident),
+        attrs,
+        ..
+    } = field
+    else {
+        return Err(Error::new(field.span(), "Expected named fields."));
+    };
+
+    let fmt = attrs
+        .into_iter()
+        .find_map(|attr| {
+            if !attr.path().is_ident("debug") {
+                return None;
+            }
+
+            let Meta::NameValue(MetaNameValue {
+                path: _,
+                eq_token: _,
+                value,
+            }) = attr.meta
+            else {
+                return None;
+            };
+
+            let Expr::Lit(ExprLit {
+                lit: Lit::Str(lit), ..
+            }) = value
+            else {
+                return Some(Err(Error::new(value.span(), "Expected a string literal.")));
+            };
+
+            Some(Ok(lit))
+        })
+        .transpose()?;
+
+    let res = if let Some(fmt) = fmt {
+        quote! { .field(::std::stringify!(#ident), &::std::format_args!(#fmt, &self.#ident)) }
+    } else {
+        quote! { .field(::std::stringify!(#ident), &self.#ident) }
+    };
+
+    Ok(res)
 }
